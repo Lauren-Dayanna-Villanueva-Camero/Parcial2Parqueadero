6b75ed3da563cf24c2655481e9cc4712123b4758
Lauren Villanueva <u20241220012@usco.edu.com>
2025-11-08 07:34:47 -0500

    Primer commit

---
+++ Parcial2Parqueadero/src/main/java/com/usco/Parcial2Parqueadero/Parcial2ParqueaderoApplication.java
@@ -0,0 +1,13 @@
+package com.usco.Parcial2Parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2ParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2ParqueaderoApplication.class, args);
+	}
+
+}
---
+++ Parcial2Parqueadero/src/test/java/com/usco/Parcial2Parqueadero/Parcial2ParqueaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usco.Parcial2Parqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ParqueaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
